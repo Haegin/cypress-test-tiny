c843dc860acae925fbce88c97b36eb7376e6f998
Harry Mills <harry@portermills.net>
2019-03-11 13:06:40 -0400

    Example tests for possibly Cypress bug
    To run tests launch Cypress with
    `CYPRESS_baseUrl='https://femalefunders.com' yarn cypress open` and run
    the tests. The failure is the same in both Chrome and Electron.

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,3 +1,23 @@
-describe('page', () => {
-  it('works', () => {})
+describe('The FemaleFunders website', () => {
+  it('Shows the home page', () => {
+    cy.visit('/')
+    cy.getByText('Empowering women to shape the future of funding.')
+  })
+
+  it('links to Angel Academy', () => {
+    cy.visit('/')
+    cy.getByText('Angel Academy').click()
+    cy.getByText(
+      'An investor accelerator program for female leaders in the corporate and technology sectors.'
+    )
+  })
+
+  it('links to the about page', () => {
+    cy.visit('/')
+    cy.getByText('About').click()
+    cy.getByText(
+      'Female Funders is an investor accelerator program for corporate and technology leaders.'
+    )
+  })
 })
+
